8d4086ec6d07aa25d47b87c9f9bead59ccf5bb72
ysjung <lf90050@linefriends.com>
2017-04-27 17:13:46 +0900

    material-ui 추가

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  (
+    <MuiThemeProvider>
+      <App />
+    </MuiThemeProvider>
+  ),
   document.getElementById('root')
 );
